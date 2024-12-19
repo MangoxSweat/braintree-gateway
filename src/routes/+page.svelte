9a086f63e3f1ceca2ba9b1ef2676af9a8a6541ad
Xavier Cooper <xcc2b@virginia.edu>
2024-12-19 15:22:30 +0000

    make styles resemble igmorefollowers add payment form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,15 +7,45 @@
 </script>
 
 <main>
-	<h1>Pay Here!</h1>
+	<h1>Add funds</h1>
 	<BraintreeFields />
+	<div id="form-images">
+		<img src="/processors.png" id="processor-image" alt="Accepted forms of payment" />
+		<img src="/secure.jpeg" id="secure-image" alt="Secure payments" />
+	</div>
 </main>
 
 <style>
+	#form-images {
+		width: 100vw;
+		text-align: center;
+		padding: 1em 0;
+	}
+	img#processor-image {
+		width: 20%;
+		margin: auto;
+		display: inline-block;
+	}
+	img#secure-image {
+		width: 10vw;
+		display: inline-block;
+	}
 	h1 {
 		margin: 10px auto;
 		width: 80%;
 		text-align: center;
+		color: rgba(12, 48, 91, 1);
+		font-family: 'Cairo', sans-serif;
+		font-style: normal;
+		font-weight: 600;
 	}
 	/* Add your global styles here */
+
+	main {
+		background: rgba(233, 242, 252, 1);
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
 </style>
