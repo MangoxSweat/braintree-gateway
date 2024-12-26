3c629c103539418f9f748f0a4556c84fdf1920b9
Xavier Cooper <xcc2b@virginia.edu>
2024-12-26 20:50:16 +0000

    taking paypal form live

--- src/lib/paypal.js
+++ src/lib/paypal.js
@@ -1,5 +1,9 @@
 // src/lib/paypal.js
 import dotenv from 'dotenv';
+dotenv.config();
+console.log('PAYPAL_LIVE_CLIENT_ID:', process.env.PAYPAL_LIVE_CLIENT_ID || 'Not Set');
+console.log('PAYPAL_LIVE_CLIENT_SECRET:', process.env.PAYPAL_LIVE_CLIENT_SECRET || 'Not Set');
+
 import {
 	ApiError,
 	Client,
@@ -9,15 +13,11 @@ import {
 	PaymentsController
 } from '@paypal/paypal-server-sdk';
 
-dotenv.config();
-
 const client = new Client({
-	clientCredentialsAuthCredentials: {
-		oAuthClientId: process.env.PAYPAL_LIVE_CLIENT_ID,
-		oAuthClientSecret: process.env.PAYPAL_LIVE_CLIENT_SECRET
-	},
+	oAuthClientId: process.env.PAYPAL_LIVE_CLIENT_ID,
+	oAuthClientSecret: process.env.PAYPAL_LIVE_CLIENT_SECRET,
 	timeout: 0,
-	environment: Environment.Live,
+	environment: Environment.Production,
 	logging: {
 		logLevel: LogLevel.Info,
 		logRequest: { logBody: true },
