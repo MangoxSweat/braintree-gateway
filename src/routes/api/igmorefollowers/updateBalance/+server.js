6e864d14ee605c10466418c345cd0796c806de1e
Xavier Cooper <xcc2b@virginia.edu>
2024-12-28 17:48:29 +0000

    adding dashboard to view logs

--- src/routes/api/igmorefollowers/updateBalance/+server.js
+++ src/routes/api/igmorefollowers/updateBalance/+server.js
@@ -1,12 +1,10 @@
 import { json } from '@sveltejs/kit';
 import axios from 'axios';
 import dotenv from 'dotenv';
-const pino = require('pino');
 dotenv.config();
 
-const logger = pino();
 async function addPayment(amt, user) {
-	logger.info('add payment igmorefollowers');
+	console.log('add payment igmorefollowers');
 	const url = 'https://igmorefollowers.com/adminapi/v2/payments/add';
 	const body = {
 		username: user,
@@ -21,17 +19,17 @@ async function addPayment(amt, user) {
 	};
 
 	try {
-		logger.info('update amount: ', amt);
+		console.log('update amount: ', amt);
 		const response = await axios.post(url, body, { headers });
 
 		if (response.error_code) {
 			throw new Error(response.error_message);
 		}
 
-		logger.info('Updated balance to: ', response.data.data.user.balance);
+		console.log('Updated balance to: ', response.data.data.user.balance);
 		return response.data.data.user.balance;
 	} catch (error) {
-		logger.error('Error adding payment igmorefollowers:', error.message);
+		console.error('Error adding payment igmorefollowers:', error.message);
 		throw new Error('Failed to add payment');
 	}
 }
@@ -47,7 +45,7 @@ export async function POST({ request }) {
 			body: { success: `Your balance successfully updated to ${balance}` }
 		});
 	} catch (error) {
-		logger.error('Failed to capture order:', error);
+		console.error('Failed to capture order:', error);
 		return json({
 			status: 500,
 			body: { error: `Failed to capture order - ${error}` }
