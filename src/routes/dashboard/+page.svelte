5b0c4e19814f0c96ec81daad1d5a2ed2bac64058
Xavier Cooper <xcc2b@virginia.edu>
2024-12-28 18:33:39 +0000

    adding dashboard to view logs

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,15 +2,8 @@
 	import { onMount } from 'svelte';
 
 	onMount(() => {});
-
-	function checkAuth() {
-		// Implement your authentication logic here
-		// For example, check if a token exists in localStorage
-		return localStorage.getItem('authToken') !== null;
-	}
 </script>
 
 <main>
 	<h1>Data from load function</h1>
-	<pre>{JSON.stringify(data, null, 2)}</pre>
 </main>
