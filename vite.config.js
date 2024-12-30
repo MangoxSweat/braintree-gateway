fda304577095d86eef6118b705c4b631f6928dd5
Xavier Cooper <xcc2b@virginia.edu>
2024-12-30 01:21:54 +0000

    enabling logs to be saved to db

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['mongodb'] // Ensures mongodb is treated as an external dependency
+	},
 	build: {
 		rollupOptions: {
-			external: ['mongodb']
+			external: ['mongodb'] // Explicitly marks mongodb as an external dependency
 		}
 	}
 });
