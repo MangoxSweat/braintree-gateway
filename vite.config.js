aba9a0fbe54407d673e546db5680976a2d3691ff
Xavier Cooper <xcc2b@virginia.edu>
2024-12-30 01:48:48 +0000

    enabling logs to be saved to db

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		external: ['mongodb'] // Exclude mongodb from SSR bundling
+	},
+	build: {
+		rollupOptions: {
+			external: ['mongodb'] // Exclude mongodb from client-side bundle
+		}
+	},
+	optimizeDeps: {
+		exclude: ['mongodb'] // Prevent pre-bundling of mongodb
+	}
 });
